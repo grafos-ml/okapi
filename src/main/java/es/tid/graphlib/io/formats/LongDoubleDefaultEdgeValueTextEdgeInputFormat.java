132aa91a7e5818be456fce308af6a3d99d833e08
Dionysios Logothetis <dl@tid.es>
2013-10-14 13:04:02 +0200

    more toy formats

--- src/main/java/es/tid/graphlib/io/formats/LongDoubleDefaultEdgeValueTextEdgeInputFormat.java
+++ src/main/java/es/tid/graphlib/io/formats/LongDoubleDefaultEdgeValueTextEdgeInputFormat.java
@@ -6,7 +6,8 @@ import java.util.regex.Pattern;
 import org.apache.giraph.io.EdgeReader;
 import org.apache.giraph.io.formats.IntNullTextEdgeInputFormat;
 import org.apache.giraph.io.formats.TextEdgeInputFormat;
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
@@ -18,43 +19,52 @@ import org.apache.hadoop.mapreduce.TaskAttemptContext;
  *
  * Each line consists of: <src id> <dst id>
  */
-public class IntIntDefaultEdgeValueTextEdgeInputFormat extends
-    TextEdgeInputFormat<IntWritable, IntWritable> {
+public class LongDoubleDefaultEdgeValueTextEdgeInputFormat extends
+    TextEdgeInputFormat<LongWritable, DoubleWritable> {
   /** Splitter for endpoints */
   private static final Pattern SEPARATOR = Pattern.compile("[\t ]");
 
   @Override
-  public EdgeReader<IntWritable, IntWritable> createEdgeReader(
+  public EdgeReader<LongWritable, DoubleWritable> createEdgeReader(
       InputSplit split, TaskAttemptContext context) throws IOException {
-    return new IntIntTextEdgeReader();
+    return new LongDoubleTextEdgeReader(context);
   }
 
   /**
    * {@link org.apache.giraph.io.EdgeReader} associated with
    * {@link IntNullTextEdgeInputFormat}.
    */
-  public class IntIntTextEdgeReader extends
+  public class LongDoubleTextEdgeReader extends
       TextEdgeReaderFromEachLineProcessed<String[]> {
+    
+    DoubleWritable defaultEdgeValue; 
+        
+    public LongDoubleTextEdgeReader(TaskAttemptContext context) {
+      defaultEdgeValue = new DoubleWritable(
+          Double.parseDouble(
+              context.getConfiguration().get("edge.default.value")));
+    }
+        
     @Override
     protected String[] preprocessLine(Text line) throws IOException {
       return SEPARATOR.split(line.toString());
     }
 
     @Override
-    protected IntWritable getSourceVertexId(String[] tokens)
+    protected LongWritable getSourceVertexId(String[] tokens)
       throws IOException {
-      return new IntWritable(Integer.parseInt(tokens[0]));
+      return new LongWritable(Long.parseLong(tokens[0]));
     }
 
     @Override
-    protected IntWritable getTargetVertexId(String[] tokens)
+    protected LongWritable getTargetVertexId(String[] tokens)
       throws IOException {
-      return new IntWritable(Integer.parseInt(tokens[1]));
+      return new LongWritable(Long.parseLong(tokens[1]));
     }
 
     @Override
-    protected IntWritable getValue(String tokens[]) throws IOException {
-      return new IntWritable(-1);
+    protected DoubleWritable getValue(String tokens[]) throws IOException {
+      return defaultEdgeValue;
     }
   }
 }
