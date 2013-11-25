4e8b20c1cd817543483d4548167783eef4c1b87a
Dionysios Logothetis <dl@tid.es>
2013-11-25 21:27:56 +0100

    more progress on svd

--- src/main/java/ml/grafos/okapi/cf/svd/Svdpp.java
+++ src/main/java/ml/grafos/okapi/cf/svd/Svdpp.java
@@ -47,22 +47,22 @@ public class Svdpp extends BasicComputation<CfLongId,
   public static final String ITERATIONS = "svd.iterations";
   /** Default value for ITERATIONS. */
   public static final int ITERATIONS_DEFAULT = 10;
-  /** User regularization parameter. */
-  public static final String LAMBDA_USER = "svd.lambda.user";
-  /** Default value for user regularization parameter */
-  public static final float LAMBDA_USER_DEFAULT = 0.01f;
-  /** User learning rate parameter */
-  public static final String GAMMA_USER = "svd.gamma.user";
-  /** Default value for user learning rate */
-  public static final float GAMMA_USER_DEFAULT = 0.005f;
-   /** Item regularization parameter. */
-  public static final String LAMBDA_ITEM = "svd.lambda.item";
-  /** Default value for item regularization parameter */
-  public static final float LAMBDA_ITEM_DEFAULT = 0.01f;
-  /** Item learning rate parameter */
-  public static final String GAMMA_ITEM = "svd.gamma.item";
-  /** Default value for item learning rate */
-  public static final float GAMMA_ITEM_DEFAULT = 0.005f;
+  /** Factor regularization parameter. */
+  public static final String FACTOR_LAMBDA = "svd.lambda.factor";
+  /** Default value for factor regularization parameter */
+  public static final float FACTOR_LAMBDA_DEFAULT = 0.01f;
+  /** Factor learning rate parameter */
+  public static final String FACTOR_GAMMA = "svd.gamma.factor";
+  /** Default value for factor learning rate */
+  public static final float FACTOR_GAMMA_DEFAULT = 0.005f;
+   /** Bias regularization parameter. */
+  public static final String BIAS_LAMBDA = "svd.lambda.bias";
+  /** Default value for bias regularization parameter */
+  public static final float BIAS_LAMBDA_DEFAULT = 0.01f;
+  /** Bias learning rate parameter */
+  public static final String BIAS_GAMMA = "svd.gamma.bias";
+  /** Default value for bias learning rate */
+  public static final float BIAS_GAMMA_DEFAULT = 0.005f;
   /** Max rating. */
   public static final String MAX_RATING = "svd.max.rating";
   /** Default maximum rating */
@@ -79,8 +79,6 @@ public class Svdpp extends BasicComputation<CfLongId,
   /** Aggregator for the computation of RMSE */
   public static final String RMSE_AGGREGATOR = "svd.rmse.aggregator";
   
-  public static final int VALUE_INDEX = 0;
-  public static final int RELATIVE_VALUE_INDEX = 1;
   
 
   /**
@@ -97,93 +95,13 @@ public class Svdpp extends BasicComputation<CfLongId,
     // Used if RMSE version or RMSE aggregator is enabled
     double rmseErr = 0d;
 
- // Sum of items relative values - Used in user
-    DoubleArrayListWritable relativeValuesSum = new DoubleArrayListWritable();
-//    if (!isItem()) {
-//      for (int i = 0; i < vectorSize; i++) {
-//        relativeValuesSum.add(i, new DoubleWritable(0d));
-//      }
-//      if (getSuperstep() > 1) {
-//        for (SvdppMessageWrapper message : messages) {
-//          relativeValuesSum = dotAddition(relativeValuesSum,
-//            message.getRelativeValue());
-//        }
-//        vertex.getValue().setRelativeValue(relativeValuesSum);
-//      }
-//    }
-//    // FOR LOOP - for each message
     for (SvdppMessageWrapper message : messages) {
-
-      // Calculate error
-//      double observed = 
-//          (double)vertex.getEdgeValue(message.getSourceId()).get();
-//      computeBaseline(vertex, lambda, gamma, err);
-//      // Change the Vertex Latent Vector based on SVD equation
-//      if (!isItem()) {
-//        // Users - supersteps 0, 2, 4, 6, ...
-//        double predicted =
-//          predictRating(vertex, message.getMessage(),
-//            message.getBaselineEstimate().get());
-//        err = observed - predicted;
-//        computeValue(vertex, lambda, gamma, err, message.getMessage());
-//      } else {
-//        // Items - supersteps 1, 3, 5, 7, ...
-//        double predicted =
-//          predictRating(vertex, message.getMessage(),
-//            message.getBaselineEstimate().get(), message.getRelativeValue(),
-//            message.getNumEdges());
-//        err = observed - predicted;
-//        computeValue(vertex, lambda, gamma, err, message.getMessage(),
-//          message.getRelativeValue(), message.getNumEdges());
-//        computeRelativeValue(vertex, lambda, gamma, err, message.getMessage());
-//      }
-//      
       rmseErr += Math.pow(err, 2);
-    } // END OF LOOP - for each message
+    } 
 
-//    haltFactor =
-//      defineFactor(vertex, factorFlag, initialValue, tolerance, rmseErr);
-
-    // If RMSE aggregator flag is true - send rmseErr to aggregator
     aggregate(RMSE_AGGREGATOR, new DoubleWritable(rmseErr));
 
-//    if (getSuperstep() == 0
-//      || (haltFactor > tolerance && getSuperstep() < iterations)) {
-//      sendMessage(vertex);
-//    }
-    
-    vertex.voteToHalt();
-  } // END OF compute()
-
-//  /**
-//   * Initialize Vertex Latent Vector.
-//   * 
-//   * @param vectorSize
-//   *          Latent Vector Size
-//   */
-//  public final void initValue(
-//      Vertex<Text, SvdppVertexValue, DoubleWritable> vertex, 
-//      final int vectorSize) {
-//    
-//    SvdppVertexValue value = new SvdppVertexValue();
-//    // Initialize Latent Vector
-//    for (int i = 0; i < vectorSize; i++) {
-//      value.setLatentVector(i, new DoubleWritable(
-//        ((Double.parseDouble(
-//          vertex.getId().toString().substring(2)) + i) % HUNDRED) / HUNDRED));
-//    }
-//    // Initialize Baseline Estimate
-//    value.setBaselineEstimate(new DoubleWritable(
-//      (Double.parseDouble(
-//        vertex.getId().toString().substring(2)) % HUNDRED) / HUNDRED));
-//    // Initialize Relative Value
-//    for (int i = 0; i < vectorSize; i++) {
-//      value.setRelativeValue(i, new DoubleWritable(
-//        ((Double.parseDouble(
-//          vertex.getId().toString().substring(2)) + i) % HUNDRED) / HUNDRED));
-//    }
-//    vertex.setValue(value);
-//  }
+  } 
 
   /**
    * Computes the predicted rating r between a user and an item based on the
@@ -222,7 +140,7 @@ public class Svdpp extends BasicComputation<CfLongId,
   }
   
   /**
-   * Computes the updated baseline vector b based on the formula:
+   * Computes the updated baseline based on the formula:
    * 
    * b := b + gamma * (error - lambda * b)
    * 
@@ -232,7 +150,7 @@ public class Svdpp extends BasicComputation<CfLongId,
    * @param gamma
    * @param lambda
    */
-  protected static final float computeUpdatedBaseLine(float baseline, 
+  protected static final float computeUpdatedBaseLine(final float baseline, 
       final float predictedRating, final float observedRating, 
       final float gamma, final float lambda) {
     
@@ -240,35 +158,36 @@ public class Svdpp extends BasicComputation<CfLongId,
         gamma*((predictedRating-observedRating)-lambda*baseline);
   }
   
-//  /**
-//   * Send messages to neighbors.
-//   */
-//  public final void sendMessage(
-//      Vertex<Text, SvdppVertexValue, DoubleWritable> vertex) {
-//    // Create a message and wrap together the source id and the message
-//    SvdppMessageWrapper message = new SvdppMessageWrapper();
-//    message.setSourceId(vertex.getId());
-//    message.setBaselineEstimate(vertex.getValue().getBaselineEstimate());
-//    message.setRelativeValue(vertex.getValue().getRelativeValue());
-//    if (!isItem()) {
-//      message.setNumEdges(new IntWritable(vertex.getNumEdges()));
-//    } else {
-//      message.setNumEdges(new IntWritable(0));
-//    }
-//    // At superstep 0, users send rating to items
-//    if (getSuperstep() == 0) {
-//      for (Edge<Text, DoubleWritable> edge : vertex.getEdges()) {
-//        DoubleArrayListWritable x = 
-//            new DoubleArrayListWritable(vertex.getValue().getLatentVector());
-//        x.add(new DoubleWritable(edge.getValue().get()));
-//        message.setMessage(x);
-//        sendMessage(edge.getTargetVertexId(), message);
-//      }
-//    } else {
-//      message.setMessage(vertex.getValue().getLatentVector());
-//      sendMessageToAllEdges(vertex, message);
-//    }
-//  }
+  /**
+   * Computes the updated baseline based on the formula:
+   * 
+   * b:= b + step - gamma*lambda*b;
+   * 
+   * @param baseline
+   * @param step
+   * @param gamma
+   * @param lambda
+   * @return
+   */
+  protected static final float incrementValue(final float baseline, 
+      final float step, final float gamma, final float lambda) {
+    return baseline+step-gamma*lambda*baseline;
+  }
+
+  /**
+   * Updates the item weight y based on the formula
+   * 
+   * v:= v + step - gamma*lambda*v
+   * 
+   * @param value
+   * @param step
+   * @param gamma
+   * @param lambda
+   */
+  protected static void incrementValue(FloatMatrix value, FloatMatrix step, 
+      final float gamma, final float lambda) {
+    value.addi(value.mul(-gamma*lambda).addi(step));
+  }
 
   /**
    * A value in the Svdpp algorithm consists of (i) the baseline estimate, (ii)
@@ -413,8 +332,10 @@ public class Svdpp extends BasicComputation<CfLongId,
   public static class UserComputation extends BasicComputation<CfLongId, 
   SvdppValue, FloatWritable, FloatMatrixMessage> {
 
-    private float lambda;
-    private float gamma;
+    private float biasLambda;
+    private float biasGamma;
+    private float factorLambda;
+    private float factorGamma;
     private float minRating;
     private float maxRating;
     private int vectorSize;
@@ -428,10 +349,14 @@ public class Svdpp extends BasicComputation<CfLongId,
     
     @Override
     public void preSuperstep() {
-      lambda = getContext().getConfiguration().getFloat(LAMBDA_USER, 
-          LAMBDA_USER_DEFAULT);
-      gamma = getContext().getConfiguration().getFloat(GAMMA_USER, 
-          GAMMA_USER_DEFAULT);
+      factorLambda = getContext().getConfiguration().getFloat(FACTOR_LAMBDA, 
+          FACTOR_LAMBDA_DEFAULT);
+      factorGamma = getContext().getConfiguration().getFloat(FACTOR_GAMMA, 
+          FACTOR_GAMMA_DEFAULT);
+      biasLambda = getContext().getConfiguration().getFloat(BIAS_LAMBDA, 
+          BIAS_LAMBDA_DEFAULT);
+      biasGamma = getContext().getConfiguration().getFloat(BIAS_GAMMA, 
+          BIAS_GAMMA_DEFAULT);
       minRating = getContext().getConfiguration().getFloat(MIN_RATING, 
           MIN_RATING_DEFAULT);
       maxRating = getContext().getConfiguration().getFloat(MAX_RATING, 
@@ -448,13 +373,17 @@ public class Svdpp extends BasicComputation<CfLongId,
         Iterable<FloatMatrixMessage> messages) throws IOException {
       
       float userBaseline = vertex.getValue().getBaseline();
+      int numRatings = vertex.getNumEdges();
+      FloatMatrixWritable userFactors = vertex.getValue().getFactors();
       
       FloatMatrix sumWeights = new FloatMatrix(1,vectorSize);
       for (FloatMatrixMessage msg : messages) {
-        // The weights are in the second row of the matrix
+        // The weights are in the 2nd row of the matrix
         sumWeights.addi(msg.getFactors().getRow(1));
       }
       
+      FloatMatrix itemWeightStep = new FloatMatrix(1,vectorSize);
+
       for (FloatMatrixMessage msg : messages) {
          // row 1 of the matrix in the message holds the item factors
         FloatMatrix itemFactors = msg.getFactors().getRow(0);
@@ -464,92 +393,147 @@ public class Svdpp extends BasicComputation<CfLongId,
         float observed = vertex.getEdgeValue(msg.getSenderId()).get();
         float predicted = computePredictedRating(
             meanRating, userBaseline, itemBaseline,
-            vertex.getValue().getFactors(), itemFactors,
-            vertex.getNumEdges(), sumWeights, minRating, maxRating);
+            userFactors, itemFactors,
+            numRatings, sumWeights, minRating, maxRating);
         float error = predicted - observed;
         
         // Update baseline
         userBaseline = computeUpdatedBaseLine(userBaseline, predicted, 
-            observed, gamma, lambda);
+            observed, biasGamma, biasLambda);
         
         // Update the value
-        updateValue(vertex.getValue().getFactors(), itemFactors, error, 
-            gamma, lambda);
+        updateValue(userFactors, itemFactors, error, factorGamma, factorLambda);
+        
+        itemWeightStep.addi(itemFactors.mul(error));
       }
       
+      vertex.getValue().setBaseline(userBaseline);
+
+      itemWeightStep.muli(factorGamma/(float)Math.sqrt(numRatings));
+
+      // Now we iterate again to get the new predictions and send the updates
+      // to each item.
+      for (FloatMatrixMessage msg : messages) {
+        FloatMatrix itemFactors = msg.getFactors().getRow(0);
+        float itemBaseline = msg.getScore();
+        float observed = vertex.getEdgeValue(msg.getSenderId()).get();
+        float predicted = computePredictedRating(
+            meanRating, userBaseline, itemBaseline,
+            userFactors, itemFactors,
+            numRatings, sumWeights, minRating, maxRating);
+        float error = predicted - observed;
+        float itemBiasStep = biasGamma*error;
+        FloatMatrix itemFactorStep = 
+            sumWeights.mul(1f/(float)Math.sqrt(numRatings)).add(
+                userFactors).mul(factorGamma*error);
+        
+        FloatMatrixWritable packedVectors = 
+            new FloatMatrixWritable(2, vectorSize);
+        packedVectors.putRow(0, itemFactorStep);
+        packedVectors.putRow(1, itemWeightStep); 
+
+        sendMessage(msg.getSenderId(), 
+            new FloatMatrixMessage(
+                vertex.getId(), packedVectors, itemBiasStep));
+      }
+
       vertex.voteToHalt();
     }
   }
   
   public static class ItemComputation extends BasicComputation<CfLongId, 
-  FloatMatrixWritable, FloatWritable, FloatMatrixMessage> {
+  SvdppValue, FloatWritable, FloatMatrixMessage> {
     
-    private float lambda;
-    private float gamma;
+    private float biasLambda;
+    private float biasGamma;
+    private float factorLambda;
+    private float factorGamma;
 
-    /**
-     * Updates the item weight y based on the formula:
-     * 
-     * y_j = y_j + gamma * (err * (1/sqrt(N) * q_i - lambda * y_j)
-     * 
-     * where
-     * y: the item weight
-     * N: the number ratings
-     * 
-     * @param weight
-     * @param item
-     * @param error
-     * @param numRatings
-     * @param gamma
-     * @param lambda
-     */
-    protected void updateWeight(FloatMatrix weight, FloatMatrix item, 
-        final float error, final int numRatings, final float gamma, 
-        final float lambda) {
-      
-      weight.addi(weight.mul(-lambda*gamma).addi(
-          item.mul(error*gamma/(float)Math.sqrt(numRatings))));
-    }
+//    /**
+//     * Updates the item weight y based on the formula:
+//     * 
+//     * y_j = y_j + gamma * (err * (1/sqrt(N) * q_i - lambda * y_j)
+//     * 
+//     * where
+//     * y: the item weight
+//     * N: the number ratings
+//     * 
+//     * @param weight
+//     * @param item
+//     * @param error
+//     * @param numRatings
+//     * @param gamma
+//     * @param lambda
+//     */
+//    protected void updateWeight(FloatMatrix weight, FloatMatrix item, 
+//        final float error, final int numRatings, final float gamma, 
+//        final float lambda) {
+//      
+//      weight.addi(weight.mul(-lambda*gamma).addi(
+//          item.mul(error*gamma/(float)Math.sqrt(numRatings))));
+//    }
     
-    /**
-     * Updates the item vector q based on the formula:
-     * q = q + gamma * (error * (p + (1/sqrt(N) * sum(y_j)) - lambda * q))
-     * 
-     * where,
-     * q: the item vector
-     * p: the user vector
-     * N: the number of ratings of the user
-     * 
-     * @param item
-     * @param user
-     * @param sumWeights
-     * @param error
-     * @param numRatings
-     * @param gamma
-     * @param lambda
-     */
-    protected void updateValue(FloatMatrix item, FloatMatrix user, 
-        FloatMatrix sumWeights, final float error, int numRatings, 
-        final float gamma, final float lambda) {
-      
-      item.addi(user.mul(gamma*error).addi(item.mul(-lambda*gamma).addi(
-          sumWeights.mul(gamma*error/(float)(Math.sqrt(numRatings))))));
-    }
+   
+    
+//    /**
+//     * Updates the item vector q based on the formula:
+//     * q = q + gamma * (error * (p + (1/sqrt(N) * sum(y_j)) - lambda * q))
+//     * 
+//     * where,
+//     * q: the item vector
+//     * p: the user vector
+//     * N: the number of ratings of the user
+//     * 
+//     * @param item
+//     * @param user
+//     * @param sumWeights
+//     * @param error
+//     * @param numRatings
+//     * @param gamma
+//     * @param lambda
+//     */
+//    protected void updateValue(FloatMatrix item, FloatMatrix user, 
+//        FloatMatrix sumWeights, final float error, int numRatings, 
+//        final float gamma, final float lambda) {
+//      
+//      item.addi(user.mul(gamma*error).addi(item.mul(-lambda*gamma).addi(
+//          sumWeights.mul(gamma*error/(float)(Math.sqrt(numRatings))))));
+//    }
+    
     
     @Override
     public void preSuperstep() {
-      lambda = getContext().getConfiguration().getFloat(Svdpp.LAMBDA_ITEM, 
-          Svdpp.LAMBDA_ITEM_DEFAULT);
-      gamma = getContext().getConfiguration().getFloat(Svdpp.GAMMA_ITEM, 
-          Svdpp.GAMMA_ITEM_DEFAULT);  
+      biasLambda = getContext().getConfiguration().getFloat(BIAS_LAMBDA, 
+          BIAS_LAMBDA_DEFAULT);
+      biasGamma = getContext().getConfiguration().getFloat(BIAS_GAMMA, 
+          BIAS_GAMMA_DEFAULT);  
+      factorLambda = getContext().getConfiguration().getFloat(FACTOR_LAMBDA, 
+          FACTOR_LAMBDA_DEFAULT);
+      factorGamma = getContext().getConfiguration().getFloat(FACTOR_GAMMA, 
+          FACTOR_GAMMA_DEFAULT);
     }
     
     @Override
     public void compute(
-        Vertex<CfLongId, FloatMatrixWritable, FloatWritable> arg0,
-        Iterable<FloatMatrixMessage> arg1) throws IOException {
-      // TODO Auto-generated method stub
+        Vertex<CfLongId, SvdppValue, FloatWritable> vertex,
+        Iterable<FloatMatrixMessage> messages) throws IOException {
       
+      float itemBaseline = vertex.getValue().getBaseline();
+      FloatMatrix itemFactors = vertex.getValue().getFactors().getRow(0);
+      FloatMatrix itemWeights = vertex.getValue().getFactors().getRow(1);
+      
+      for (FloatMatrixMessage msg : messages) {
+        float itemBiasStep = msg.getScore();
+        FloatMatrix itemFactorStep = msg.getFactors().getRow(0);
+        FloatMatrix itemWeightStep = msg.getFactors().getRow(1);
+        
+        itemBaseline = incrementValue(itemBaseline, itemBiasStep, biasGamma, 
+            biasLambda);
+        incrementValue(itemFactors, itemFactorStep, factorGamma, factorLambda);
+        incrementValue(itemWeights, itemWeightStep, factorGamma, factorLambda);
+      }
+      
+      vertex.getValue().setBaseline(itemBaseline);
     }
   }
   
--- src/test/java/ml/grafos/okapi/cf/svd/SvdppTest.java
+++ src/test/java/ml/grafos/okapi/cf/svd/SvdppTest.java
@@ -34,45 +34,45 @@ public class SvdppTest {
         0.000001f );
   }
   
-  @Test
-  public void testItemUpdate() {
-    float lambda = 0.01f;
-    float gamma = 0.005f;
-    float error = 1f;
-    int numRatings = 10;
-    
-    //user = (0.1, 0.2, 0.3)
-    FloatMatrix user = new FloatMatrix(1, 3, new float[]{0.1f, 0.2f, 0.3f});
-    //item = (0.2, 0.1, 0.4)
-    FloatMatrix item = new FloatMatrix(1, 3, new float[]{0.2f, 0.1f, 0.4f});
-    //weights = (0.4, 0.6, 0.8)
-    FloatMatrix weights = new FloatMatrix(1, 3, new float[]{0.4f, 0.6f, 0.8f});
-    
-    Svdpp.ItemComputation comp = new Svdpp.ItemComputation();
-    comp.updateValue(item, user, weights, error, numRatings, gamma, lambda);
-    
-    assertArrayEquals(item.data, new float[] {
-        0.201122455532034f, 0.101943683298051f, 0.402744911064067f}, 0.000001f);
-  }
+//  @Test
+//  public void testItemUpdate() {
+//    float lambda = 0.01f;
+//    float gamma = 0.005f;
+//    float error = 1f;
+//    int numRatings = 10;
+//    
+//    //user = (0.1, 0.2, 0.3)
+//    FloatMatrix user = new FloatMatrix(1, 3, new float[]{0.1f, 0.2f, 0.3f});
+//    //item = (0.2, 0.1, 0.4)
+//    FloatMatrix item = new FloatMatrix(1, 3, new float[]{0.2f, 0.1f, 0.4f});
+//    //weights = (0.4, 0.6, 0.8)
+//    FloatMatrix weights = new FloatMatrix(1, 3, new float[]{0.4f, 0.6f, 0.8f});
+//    
+//    Svdpp.ItemComputation comp = new Svdpp.ItemComputation();
+//    comp.updateValue(item, user, weights, error, numRatings, gamma, lambda);
+//    
+//    assertArrayEquals(item.data, new float[] {
+//        0.201122455532034f, 0.101943683298051f, 0.402744911064067f}, 0.000001f);
+//  }
   
-  @Test
-  public void testItemWeightUpdate() {
-    float lambda = 0.01f;
-    float gamma = 0.005f;
-    float error = 1f;
-    int numRatings = 10;
-    
-    //weight = (0.1, 0.2, 0.3)
-    FloatMatrix weight = new FloatMatrix(1, 3, new float[]{0.1f, 0.2f, 0.3f});
-    //item = (0.2, 0.1, 0.4)
-    FloatMatrix item = new FloatMatrix(1, 3, new float[]{0.2f, 0.1f, 0.4f});
-    
-    Svdpp.ItemComputation comp = new Svdpp.ItemComputation();
-    comp.updateWeight(weight, item, error, numRatings, gamma, lambda);
-    
-    assertArrayEquals(weight.data, new float[] {
-        0.100311227766017f, 0.200148113883008f, 0.300617455532034f}, 0.000001f);
-  }
+//  @Test
+//  public void testItemWeightUpdate() {
+//    float lambda = 0.01f;
+//    float gamma = 0.005f;
+//    float error = 1f;
+//    int numRatings = 10;
+//    
+//    //weight = (0.1, 0.2, 0.3)
+//    FloatMatrix weight = new FloatMatrix(1, 3, new float[]{0.1f, 0.2f, 0.3f});
+//    //item = (0.2, 0.1, 0.4)
+//    FloatMatrix item = new FloatMatrix(1, 3, new float[]{0.2f, 0.1f, 0.4f});
+//    
+//    Svdpp.ItemComputation comp = new Svdpp.ItemComputation();
+//    comp.updateWeight(weight, item, error, numRatings, gamma, lambda);
+//    
+//    assertArrayEquals(weight.data, new float[] {
+//        0.100311227766017f, 0.200148113883008f, 0.300617455532034f}, 0.000001f);
+//  }
   
   @Test
   public void testUpdateBaseline() {
@@ -82,8 +82,7 @@ public class SvdppTest {
     float gamma = 0.005f;
     float lambda = 0.01f;
     
-    Svdpp svd = new Svdpp();
-    float newBaseline = svd.computeUpdatedBaseLine(baseline, predictedRating, 
+    float newBaseline = Svdpp.computeUpdatedBaseLine(baseline, predictedRating, 
         observedRating, gamma, lambda);
     
     assertEquals(newBaseline, 0.50475, 0.001f);
@@ -104,14 +103,13 @@ public class SvdppTest {
     //weights = (0.4, 0.6, 0.8)
     FloatMatrix weights = new FloatMatrix(1, 3, new float[]{0.4f, 0.6f, 0.8f});
     
-    Svdpp svd = new Svdpp();
-    float prediction = svd.computePredictedRating(meanRating, userBaseline, 
+    float prediction = Svdpp.computePredictedRating(meanRating, userBaseline, 
         itemBaseline, user, item, numRatings, weights, minRating, maxRating);
     
     assertEquals(prediction, 5.0f , 0.000001f);
     
     userBaseline = -2;
-    prediction = svd.computePredictedRating(meanRating, userBaseline, 
+    prediction = Svdpp.computePredictedRating(meanRating, userBaseline, 
         itemBaseline, user, item, numRatings, weights, minRating, maxRating);
 
     assertEquals(prediction, 3.305464f , 0.000001f);
