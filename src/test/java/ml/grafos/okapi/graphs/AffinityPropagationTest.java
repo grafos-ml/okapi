291e63cb30cd2a264731c4cd23f0e684126f69b4
Marc Pujol-Gonzalez <mpujol@iiia.csic.es>
2014-04-08 20:47:26 +0200

    Result expectations for AP test cases

--- src/test/java/ml/grafos/okapi/graphs/AffinityPropagationTest.java
+++ src/test/java/ml/grafos/okapi/graphs/AffinityPropagationTest.java
@@ -15,17 +15,20 @@
  */
 package ml.grafos.okapi.graphs;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Ordering;
+import com.google.common.io.Resources;
 import org.apache.giraph.conf.GiraphConfiguration;
 import org.apache.giraph.utils.InternalVertexRunner;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.*;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertArrayEquals;
 
 public class AffinityPropagationTest {
   private GiraphConfiguration conf;
@@ -48,17 +51,21 @@ public class AffinityPropagationTest {
       "2 1 1 3",
       "3 5 3 1",
     };
+    String[] expected = {
+      "1\t3", "2\t3", "3\t3"
+    };
 
     conf.setVertexInputFormatClass(AffinityPropagation.APVertexInputFormatter.class);
 
-    Iterable<String> results;
+    ImmutableList<String> results;
     try {
-      results = InternalVertexRunner.run(conf, graph, null);
+      results = Ordering.natural().immutableSortedCopy(
+          InternalVertexRunner.run(conf, graph, null));
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
 
-    System.err.println(results);
+    assertArrayEquals(expected, results.toArray());
   }
 
   @Test
@@ -70,21 +77,25 @@ public class AffinityPropagationTest {
       "2 1 1",
       "2 2 1",
       "2 3 3",
-      "3 1 5",
+      "3 1 4",
       "3 2 3",
       "3 3 1",
+    };
+    String[] expected = {
+      "1\t3", "2\t3", "3\t3"
     };
 
     conf.setEdgeInputFormatClass(AffinityPropagation.APEdgeInputFormatter.class);
 
-    Iterable<String> results;
+    ImmutableList<String> results;
     try {
-      results = InternalVertexRunner.run(conf, null, graph);
+      results = Ordering.natural().immutableSortedCopy(
+          InternalVertexRunner.run(conf, null, graph));
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
 
-    System.err.println(results);
+    assertArrayEquals(expected, results.toArray());
   }
 
   @Test
@@ -95,45 +106,52 @@ public class AffinityPropagationTest {
       "1 3 5",
       "2 1 1",
       "2 2 1",
-      "3 1 5",
+      "3 1 4",
       "3 3 1",
+    };
+    String[] expected = {
+        "1\t3", "2\t2", "3\t3"
     };
 
     conf.setEdgeInputFormatClass(AffinityPropagation.APEdgeInputFormatter.class);
 
-    Iterable<String> results;
+    ImmutableList<String> results;
     try {
-      results = InternalVertexRunner.run(conf, null, graph);
+      results = Ordering.natural().immutableSortedCopy(
+          InternalVertexRunner.run(conf, null, graph));
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
 
-    System.err.println(results);
+    assertArrayEquals(expected, results.toArray());
   }
 
   @Test
   public void testToyProblem() throws IOException {
-    InputStream is = getClass().getResourceAsStream("/ap/toyProblem.txt");
-    BufferedReader br = new BufferedReader(new InputStreamReader(is));
+    String[] expected = {
+        "1\t3", "2\t3", "3\t3", "4\t3", "5\t3", "6\t3", "7\t7",
+        "8\t7", "9\t7", "10\t7", "11\t3", "12\t7", "13\t3", "14\t7",
+        "15\t7", "16\t20", "17\t20", "18\t20", "19\t20", "20\t20",
+        "21\t20", "22\t3", "23\t20", "24\t20", "25\t7",
+    };
+    Arrays.sort(expected, Ordering.natural());
 
-    String line;
-    List<String> lines = new ArrayList<String>();
-    while((line = br.readLine()) != null) {
-      lines.add(line);
-    }
-
+    List<String> lines = Resources.readLines(
+        Resources.getResource(getClass(), "toyProblem.txt"),
+        StandardCharsets.UTF_8);
     String[] graph = lines.toArray(new String[0]);
 
     conf.setEdgeInputFormatClass(AffinityPropagation.APEdgeInputFormatter.class);
 
-    Iterable<String> results;
+    ImmutableList<String> results;
     try {
-      results = InternalVertexRunner.run(conf, null, graph);
+      results = Ordering.natural().immutableSortedCopy(
+          InternalVertexRunner.run(conf, null, graph));
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
 
-    System.err.println(results);
+    assertArrayEquals(expected, results.toArray());
   }
 
 }
