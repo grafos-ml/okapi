c88567370003acf89609573bf422e9031dde2a59
Toni Penya-Alba <tonipenya@iiia.csic.es>
2014-04-09 18:32:11 +0200

    moved AffinityPropagation to a clustering package.

--- src/main/java/ml/grafos/okapi/clustering/ap/AffinityPropagation.java
+++ src/main/java/ml/grafos/okapi/clustering/ap/AffinityPropagation.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ml.grafos.okapi.graphs;
+package ml.grafos.okapi.clustering.ap;
 
 import com.google.common.collect.ComparisonChain;
 import es.csic.iiia.bms.CommunicationAdapter;
--- src/test/java/ml/grafos/okapi/clustering/ap/AffinityPropagationTest.java
+++ src/test/java/ml/grafos/okapi/clustering/ap/AffinityPropagationTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ml.grafos.okapi.graphs;
+package ml.grafos.okapi.clustering.ap;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Ordering;
